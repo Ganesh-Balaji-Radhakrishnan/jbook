6baad8254363b953a6d3843171ba93d01c628a5e
Ganesh Balaji Radhakrishnan <ganesh.balaji@brighter.se>
2022-11-24 14:00:49 +0100

    creating a proj to replicate codesandbox

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState, useEffect, useRef } from "react";
+import ReactDOM  from "react-dom/client";
+import * as esbuild from "esbuild-wasm"
+import {unpkgPathPlugin} from './plugins/unpkg-path-plugin'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const App:React.FC = ( ) => {
+    const [input, setInput] = useState('')
+    const [code, setCode] = useState('')
+    const ref = useRef<any>()
+
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        })
+    }
+
+    useEffect(()=>{
+        startService()
+    }, [])
+
+    const onClick = async ( ) => {
+        if(!ref.current){
+            return
+        }
+        /* const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015',
+        }) */
+
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
+        })
+
+        setCode(result.outputFiles[0].text)
+    }
+
+    return <div>
+        <textarea value={input} onChange={e=>setInput(e.target.value)}></textarea>
+        <div><button onClick={onClick}>submit</button></div>
+        <pre>{code}</pre>
+    </div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+     <App />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
